5baef7e9524b6f3a9e50a8425b2b098f2cd1af9c
Kris Casper <sw.darkwind@gmail.com>
2020-09-29 16:53:04 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,36 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
-};
+}
+
+function getChar(code) {
+    let morseCode = []
+    //get morse code
+    for(let i = 0; i < code.length; i += 2) {
+        let sub = code.substring(i, i + 2)
+        if(sub == '10') morseCode.push('.')
+        else if(sub == '11') morseCode.push('-')
+    }
+    return MORSE_TABLE[morseCode.join('')]
+}
 
 function decode(expr) {
-    // write your solution here
+    let words = expr.split('**********')
+    
+    // get word in phrase
+    for(let wordNumber = 0; wordNumber < words.length; wordNumber++) {
+        let chars = []
+        
+        //get char in word
+        for(let charNumber = 0; charNumber < words[wordNumber].length / 10; charNumber++) {
+            let code = words[wordNumber].substring(charNumber * 10, 10 * ( charNumber + 1 ))
+            chars.push(getChar(code))
+        }
+
+        words[wordNumber] = chars.join('')
+    }
+
+    return words.join(' ')
 }
 
 module.exports = {
